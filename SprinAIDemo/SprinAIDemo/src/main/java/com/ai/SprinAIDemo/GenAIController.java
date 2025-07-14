8df8ff033bd2ce55a9c0a3db46bdb69803095c2b
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-07-14 21:15:08 +0530

    gnerate image

--- SprinAIDemo/SprinAIDemo/src/main/java/com/ai/SprinAIDemo/GenAIController.java
+++ SprinAIDemo/SprinAIDemo/src/main/java/com/ai/SprinAIDemo/GenAIController.java
@@ -1,16 +1,22 @@
 package com.ai.SprinAIDemo;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class GenAIController {
 
-    ChatServices chatServices;
+    private final ChatServices chatServices;
+    private final ImageServices imageServices;
 
-    public GenAIController(ChatServices chatServices) {
+    public GenAIController(ChatServices chatServices, ImageServices imageServices) {
         this.chatServices = chatServices;
+        this.imageServices = imageServices;
     }
 
     @GetMapping("ask-ai")
@@ -22,4 +28,12 @@ public class GenAIController {
     public String getResponseOptions(@RequestParam String prompt){
         return chatServices.getResponseOptions(prompt);
     }
+
+    @GetMapping("generate-image")
+    public void generateImage(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageServices.generateImage(prompt);
+        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
+    }
+
 }
